e3a5cb080daf2c10d8f8911673878383dda168d2
Ronny <ronnychamba96@gmail.com>
2022-03-09 14:12:11 -0500

    Bean Scopes

--- src/main/java/com/curso/spring/TestPersona.java
+++ src/main/java/com/curso/spring/TestPersona.java
@@ -8,6 +8,13 @@ public class TestPersona {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/applicationContext.xml");
 
+		// Obtener bean miDireccion
+	    Direccion direccion = context.getBean("miDireccion", Direccion.class);
+	    
+	    direccion.setNumeroCasa(100);	    
+	    System.out.println(direccion.toString());
+	    
+	    
 		Persona persona = context.getBean("miPersona", Persona.class);
 		persona.verInformacion();
 		context.close();
